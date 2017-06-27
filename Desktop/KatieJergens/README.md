9d767921a65a6e191701be90acc3441cafa40532
Katie Jergens <katie.jergens@gmail.com>
2017-06-27 14:16:41 -0400

    initial commit

---
+++ Desktop/KatieJergens/README.md
@@ -0,0 +1,9 @@
+# All Star Code
+
+This is my technical assignment from All Star Code, Summer Initiative 2017.
+
+<a href="https://kjergens.github.io/allstarcode/">Memory Game</a>
+
+It uses graphics I made using Sketch 3.
+
+
---
+++ Desktop/KatieJergens/js/footer.js
@@ -0,0 +1,11 @@
+var footer = "<br>" +
+   "<h4>katie.jergens@gmail.com</h4>" +
+   "<h4><a href=\"aboutme.html\" target=\"blank\">About me</a></h4>" +
+   "<h4><a href=\"https://www.linkedin.com/in/katie-jergens-29695aa\" target=\"blank\">LinkedIn profile</a></h4>" +
+    "<h4><a href=\"https://twitter.com/KatieJergens\" target=\"blank\">Twitter feed</a></h4>" +  
+    "<br><br>Site designed & developed by Katie Jergens";
+
+
+$(function() {
+	document.getElementById("footer").innerHTML = footer;
+});
